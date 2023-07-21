a1226edc594dcc136a14e15eb540be3264ed25ed
vlad <supervl2011@yandex.ru>
2023-07-21 13:25:44 +0300

    feature: add normalize.css & css directory

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
+import './styles/index.css';
+import './styles/normalize.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
